47fcd531c0a52dd7ab303679f727a5f5cef8b2d4
Georgefarah825 <162799743+Georgefarah825@users.noreply.github.com>
2024-03-17 21:05:48 +0300

    Update task_list.dart

--- lib/widgetsfolder/task_list.dart
+++ lib/widgetsfolder/task_list.dart
@@ -1,1 +1,33 @@
 
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/tasks.dart';
+import 'package:todoey_flutter/widgetsFolder/task_tile.dart';
+
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+
+class taskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskdata, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            Tasks task = taskdata.tasks[index];
+            return tasktile(
+              isChecked: task.isDone,
+              tasketitle: task.name,
+              checkboxcallback: (bool) {
+                taskdata.updateTask(task);
+              },
+              longpressCallback: () {
+                taskdata.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskdata.taskCount,
+        );
+      },
+    );
+  }
+}
